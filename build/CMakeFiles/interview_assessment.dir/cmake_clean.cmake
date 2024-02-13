90ffa4ee3c18e3266181ad5bd532147193c0c2c4
swapnil.nerkar <swapnil.nerkar@miko2.ai>
2024-02-13 07:58:16 +0530

    cmake list added

---
+++ build/CMakeFiles/interview_assessment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interview_assessment.dir/interview_assessment.c.o"
+  "build/interview_assessment"
+  "build/interview_assessment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/interview_assessment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
